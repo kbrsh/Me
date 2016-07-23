3dfd8823d2bc0908e9dc71e0ebdb5082d82dab41
Gopal Shah <simplecooldude1@gmail.com>
2016-07-23 16:59:03 -0700

    add menulink click exit to all menulinks, not first child

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,15 @@ document.querySelector('.button_container').addEventListener("click", function()
    document.querySelector('.overlay').classList.toggle('open');
  });
 
- document.querySelector(".menuLink").addEventListener("click", function() {
-   document.querySelector('.button_container').classList.toggle('active');
-   document.querySelector('.overlay').classList.toggle('open');
- });
+ var menuLinks = document.querySelectorAll(".menuLink");
+
+ for(var i = 0; i < menuLinks.length; i++) {
+   menuLinks[i].addEventListener("click", function() {
+     document.querySelector('.button_container').classList.toggle('active');
+     document.querySelector('.overlay').classList.toggle('open');
+   });
+ }
+
 
 
 
