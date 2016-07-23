b74dfbe88c7b057f4423dd3238e4707303ca8c64
Gopal Shah <simplecooldude1@gmail.com>
2016-07-23 12:04:08 -0700

    ctrl A + backspace

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,14 @@
-/* Util */
-function hasClass(el, cls) {
-  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
-}
-
-
 /* Nav */
-$('.button_container').click(function() {
-   $('.button_container').toggleClass('active');
-   $('.overlay').toggleClass('open');
+document.querySelectorAll('.button_container').addEventListener("click", function() {
+   document.querySelectorAll('.button_container').classList.toggle('active');
+   document.querySelectorAll('.overlay').classList.toggle('open');
  });
 
- $(".menuLink").click(function() {
-   $('.button_container').toggleClass('active');
-   $('.overlay').toggleClass('open');
+ document.querySelectorAll(".menuLink").addEventListener("click", function() {
+   document.querySelectorAll('.button_container').classList.toggle('active');
+   document.querySelectorAll('.overlay').classList.toggle('open');
  });
 
-
-
-
-
-/* Window Leave Title Thing */
-
-function userLeft() {
-  document.title = "I Miss You! | Kabir Shah";
-}
-
-function userBack() {
-  document.title = "Kabir Shah";
-}
-
-  var hidden = "hidden";
-
-  // Standards:
-  if (hidden in document)
-    document.addEventListener("visibilitychange", onchange);
-  else if ((hidden = "mozHidden") in document)
-    document.addEventListener("mozvisibilitychange", onchange);
-  else if ((hidden = "webkitHidden") in document)
-    document.addEventListener("webkitvisibilitychange", onchange);
-  else if ((hidden = "msHidden") in document)
-    document.addEventListener("msvisibilitychange", onchange);
-  // IE 9 and lower:
-  else if ("onfocusin" in document)
-    document.onfocusin = document.onfocusout = onchange;
-  // All others:
-  else
-    window.onpageshow = window.onpagehide
-    = window.onfocus = window.onblur = onchange;
-
-  function onchange (evt) {
-    var v = "visible", h = "hidden",
-        evtMap = {
-          focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
-        };
-
-    evt = evt || window.event;
-    if (evt.type in evtMap)
-      document.body.className = evtMap[evt.type];
-    else
-      document.body.className = this[hidden] ? "hidden" : "visible";
-  }
-
-  // set the initial state (but only if browser supports the Page Visibility API)
-  if( document[hidden] !== undefined )
-    onchange({type: document[hidden] ? "blur" : "focus"});
-
-function check() {
-  if(hasClass(document.body, "hidden")) {
-    userLeft();
-  } else if(hasClass(document.body, "visible")) {
-    userBack();
-  }
-}
-
-setInterval(check, 500)
 
 
 /* Random Highlight */
@@ -83,35 +18,83 @@ var pick=~~(Math.random()*359),
   tag.innerHTML=style.replace(/\$pick/g,pick);
   document.body.appendChild(tag);
 
-/* Easter Eggs */
 
-  var pressedK = [];
-  var konamiCode = '38,38,40,40,37,39,37,39,66,65';
-  // Listen for key presses and record their codes in the "pressed" array.
-  window.addEventListener('keydown', function(k) {
-    pressedK.push(k.keyCode);
-    // If the user enters the "KonamiCode" sequence...
-    if (pressedK.toString().indexOf(konamiCode) >= 0) {
-      surpriseK();
-      pressedK = [];
-    }
-  }, true);
-var surpriseK = function() {
-  alert('Try Editing the Text ;)')
-  document.getElementsByTagName("HTML")[0].setAttribute("contenteditable", "true");
-};
+  (function() {
 
-var pressedM = [];
-var musicCode = '77,85,83,73,67';
-// Listen for key presses and record their codes in the "pressed" array.
-window.addEventListener('keydown', function(k) {
-  pressedM.push(k.keyCode);
-  // If the user enters the "KonamiCode" sequence...
-  if (pressedM.toString().indexOf(musicCode) >= 0) {
-    surpriseM();
-    pressedM = [];
-  }
-}, true);
-var surpriseM = function() {
-  alert('Coming Soon');
-};
+       'use strict';
+
+      // Feature Test
+      if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+
+          // Function to animate the scroll
+          var smoothScroll = function (anchor, duration) {
+
+              // Calculate how far and how fast to scroll
+              var startLocation = window.pageYOffset;
+              var endLocation = anchor.offsetTop;
+              var distance = endLocation - startLocation;
+              var increments = distance/(duration/16);
+              var stopAnimation;
+
+              // Scroll the page by an increment, and check if it's time to stop
+              var animateScroll = function () {
+                  window.scrollBy(0, increments);
+                  stopAnimation();
+              };
+
+              // If scrolling down
+              if ( increments >= 0 ) {
+                  // Stop animation when you reach the anchor OR the bottom of the page
+                  stopAnimation = function () {
+                      var travelled = window.pageYOffset;
+                      if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                          clearInterval(runAnimation);
+                      }
+                  };
+              }
+              // If scrolling up
+              else {
+                  // Stop animation when you reach the anchor OR the top of the page
+                  stopAnimation = function () {
+                      var travelled = window.pageYOffset;
+                      if ( travelled <= (endLocation || 0) ) {
+                          clearInterval(runAnimation);
+                      }
+                  };
+              }
+
+              // Loop the animation function
+              var runAnimation = setInterval(animateScroll, 16);
+
+          };
+
+          // Define smooth scroll links
+          var scrollToggle = document.querySelectorAll('.scroll');
+
+          // For each smooth scroll link
+          [].forEach.call(scrollToggle, function (toggle) {
+
+              // When the smooth scroll link is clicked
+              toggle.addEventListener('click', function(e) {
+
+                  // Prevent the default link behavior
+                  e.preventDefault();
+
+                  // Get anchor link and calculate distance from the top
+                  var dataID = toggle.getAttribute('href');
+                  var dataTarget = document.querySelector(dataID);
+                  var dataSpeed = toggle.getAttribute('data-speed');
+
+                  // If the anchor exists
+                  if (dataTarget) {
+                      // Scroll to the anchor
+                      smoothScroll(dataTarget, dataSpeed || 500);
+                  }
+
+              }, false);
+
+          });
+
+      }
+
+   })();
