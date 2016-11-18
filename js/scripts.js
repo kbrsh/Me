3d03321c163cef807248f9b66fc251cdf46f9f7a
Gopal Shah <simplecooldude1@gmail.com>
2016-11-17 18:30:17 -0800

    no random highlight

--- js/scripts.js
+++ js/scripts.js
@@ -27,11 +27,11 @@ document.querySelector('.burger').addEventListener("click", function() {
 
 
 /* Random Highlight */
-var pick=~~(Math.random()*359),
-    tag=document.createElement('style'),
-    style='::-moz-selection {color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
-  tag.innerHTML=style.replace(/\$pick/g,pick);
-  document.body.appendChild(tag);
+// var pick=~~(Math.random()*359),
+//     tag=document.createElement('style'),
+//     style='::-moz-selection {color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
+//   tag.innerHTML=style.replace(/\$pick/g,pick);
+//   document.body.appendChild(tag);
 
 
 // // Image effects + Color Change
