646e7836101565f35c7726bb4396a1c9a43cf786
Gopal Shah <simplecooldude1@gmail.com>
2017-01-13 18:33:25 -0800

    remove comments

--- js/scripts.js
+++ js/scripts.js
@@ -24,57 +24,6 @@ document.querySelector('.burger').addEventListener("click", function() {
  }
 
 
-
-
-/* Random Highlight */
-// var pick=~~(Math.random()*359),
-//     tag=document.createElement('style'),
-//     style='::-moz-selection {color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
-//   tag.innerHTML=style.replace(/\$pick/g,pick);
-//   document.body.appendChild(tag);
-
-
-// // Image effects + Color Change
-// function elementInViewport(el) {
-//     return (el.getBoundingClientRect().top >= 0) && (el.getBoundingClientRect().bottom >= 0);
-// }
-//
-// function topInViewport(el) {
-//   return el.getBoundingClientRect().top < window.innerHeight;
-// }
-//
-// function inView(el) {
-//
-// }
-
-// document.addEventListener("scroll", function(e) {
-//   // var els = document.getElementsByClassName("lax-img");
-//   // var scrollTop = document.body.scrollTop;
-//   // for (var i = 0; i < els.length; i++) {
-//   //   var el = els[i];
-//   //   var elemTop = el.getBoundingClientRect().top;
-//   //   var elemBottom = el.getBoundingClientRect().bottom;
-//   //   var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-//   //   if ((scrollTop + window.innerHeight - el.offsetTop - 100) / 4 > 0) {
-//   //     var calc = (scrollTop + window.innerHeight - el.offsetTop - 100) / 4;
-//   //     el.style.transform = 'translateY(-' + calc + 'px)'
-//   //   }
-//   // }
-//
-//
-//   if(topInViewport(document.getElementById("projects"))) {
-//     document.body.classList.add("dark");
-//   } else if(topInViewport(document.getElementById("about"))) {
-//     document.body.classList.remove("dark");
-//   } else if(topInViewport(document.getElementById("contact"))) {
-//     document.body.classList.remove("dark");
-//   }
-//
-//
-//
-// });
-
-
 /* smooth scroll */
   (function() {
 
