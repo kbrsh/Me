1cfc671e90413e93c31354f532943043246adffa
Kabir Shah <simplecooldude1@gmail.com>
2017-07-27 12:27:57 -0700

    faster smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var burger = document.getElementsByClassName('burger')[0];
 var overlay = document.getElementsByClassName('overlay')[0];
 
-var toggleMenu = function() {
+var toggleMenu = function(e) {
   burger.classList.toggle('active');
   overlay.classList.toggle('open');
 }
@@ -15,4 +15,32 @@ for(var i = 0; i < menuLinks.length; i++) {
 }
 
 // Smooth scrolling
+var scroll = function(el) {
+  return function(e) {
+    e.preventDefault();
+
+    var offset = window.scrollY;
+    var distance = (el.offsetTop - offset);
+    var end = distance + offset;
+    var left = distance % 50;
+
+    distance -= left;
+    window.scrollBy(0, left);
+
+    var increment = distance / 50;
+
+    var interval = setInterval(function() {
+      window.scrollBy(0, increment);
+      if(window.scrollY === end) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
+}
+
 var scrollLinks = document.getElementsByClassName("scroll");
+for(var i = 0; i < scrollLinks.length; i++) {
+  var link = scrollLinks[i];
+  var el = document.getElementById(link.href.split("#")[1]);
+  link.addEventListener("click", scroll(el));
+}
