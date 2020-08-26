39bdac695992a568fe1495b0961ebf744abd9177
Kabir Shah <iamkbrsh@gmail.com>
2020-08-26 16:47:44 -0700

    persist theme

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,10 +1,11 @@
 var themes = ["light", "dark", "topography", "image"];
-var themeIndex = 0;
+var themeIndex = parseInt(localStorage.kabirThemeIndex);
 var html = document.documentElement;
 var logo = document.getElementById("logo");
 
 function updateTheme() {
 	var theme = themes[themeIndex];
+	localStorage.kabirThemeIndex = themeIndex;
 
 	switch (theme) {
 		case "light": {
@@ -34,14 +35,15 @@ function updateTheme() {
 	}
 }
 
-if (matchMedia("(prefers-color-scheme: dark)").matches) {
-	themeIndex = 1;
-	updateTheme();
-} else {
-	themeIndex = 0;
-	updateTheme();
+if (isNaN(themeIndex)) {
+	if (matchMedia("(prefers-color-scheme: dark)").matches) {
+		themeIndex = 1;
+	} else {
+		themeIndex = 0;
+	}
 }
 
+updateTheme();
 window.addEventListener("keydown", function(event) {
 	if (event.key === "t") {
 		themeIndex = (themeIndex + 1) % themes.length;
