5eb565f8438774ba6895ff9610e2b3e0070b7f3c
Kabir Shah <iamkbrsh@gmail.com>
2020-08-26 19:31:39 -0700

    make favicon not depend on theme

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,9 @@
 var themes = ["light", "dark", "topography", "image"];
 var themeIndex = parseInt(localStorage.kabirThemeIndex);
+var favicon = document.getElementById("favicon");
 var html = document.documentElement;
 var logo = document.getElementById("logo");
+var isDarkTheme = matchMedia("(prefers-color-scheme: dark)").matches;
 
 function updateTheme() {
 	var theme = themes[themeIndex];
@@ -11,32 +13,34 @@ function updateTheme() {
 		case "light": {
 			html.className = "theme-light";
 			logo.src = "/images/light-logo.svg";
-			document.getElementById("favicon").href = "/images/favicon-dark.png";
 			break;
 		}
 		case "dark": {
 			html.className = "theme-dark";
 			logo.src = "/images/dark-logo.svg";
-			document.getElementById("favicon").href = "/images/favicon-light.png";
 			break;
 		}
 		case "topography": {
 			html.className = "theme-topography";
 			logo.src = "/images/topography-logo.svg";
-			document.getElementById("favicon").href = "/images/favicon-light.png";
 			break;
 		}
 		case "image": {
 			html.className = "theme-image";
 			logo.src = "/images/image-logo.svg";
-			document.getElementById("favicon").href = "/images/favicon-light.png";
 			break;
 		}
 	}
 }
 
+if (isDarkTheme) {
+	favicon.href = "/images/favicon-light.png";
+} else {
+	favicon.href = "/images/favicon-dark.png";
+}
+
 if (isNaN(themeIndex)) {
-	if (matchMedia("(prefers-color-scheme: dark)").matches) {
+	if (isDarkTheme) {
 		themeIndex = 1;
 	} else {
 		themeIndex = 0;
