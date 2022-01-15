15705e94e25c448c5aaf1c416454ac24189be4ee
Kabir Shah <iamkbrsh@gmail.com>
2022-01-15 01:13:46 -0800

    begin

--- public/js/index.js
+++ public/js/index.js
@@ -1,82 +1,215 @@
-var canvas = document.getElementById("canvas")
-var gl = canvas.getContext("webgl")
-var seed = Math.random()*7
-var looping = false
-var shaderVertex, shaderGradient, widthHandle, heightHandle, seedHandle
+const lerp = (x, min, max) => x * (max - min) + min
 
-function update() {
-	canvas.width = canvas.offsetWidth
-	canvas.height = canvas.offsetHeight
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-	gl.clearColor(0, 0, 0, 0)
-	gl.clear(gl.COLOR_BUFFER_BIT)
-	gl.uniform1f(widthHandle, gl.canvas.width)
-	gl.uniform1f(heightHandle, gl.canvas.height)
-	gl.uniform1f(seedHandle, seed+=0.001)
-	gl.drawArrays(gl.TRIANGLES, 0, 6)
+const makeGrid = (width, height) => {
+   const grid = []
+
+   for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+         grid.push({
+            x: lerp(x / (width - 1), -1, 1),
+            y: lerp(y / (height - 1), -1, 1),
+            u: 2 / (width - 1),
+            v: 2 / (height - 1),
+            h: Math.random(),
+            s: Math.random(),
+            l: Math.random(),
+         })
+      }
+   }
+
+   return grid
 }
 
-function loop() {
-	update()
+const Mh = new THREE.Matrix4()
+// prettier-ignore
+Mh.set(
+   2, -2, 1, 1,
+   -3, 3, -2, -1,
+   0, 0, 1, 0,
+   1, 0, 0, 0
+);
 
-	if (looping)
-		requestAnimationFrame(loop)
+const MhT = Mh.clone().transpose()
+
+const dividePoint = (u, v, x, y, h, s, l) => {
+   const vp = new THREE.Vector4(v * v * v, v * v, v, 1)
+
+   const xp = vp.dot(
+      new THREE.Vector4(u * u * u, u * u, u, 1).applyMatrix4(
+         x.clone().transpose().premultiply(Mh).multiply(MhT)
+      )
+   )
+
+   const yp = vp.dot(
+      new THREE.Vector4(u * u * u, u * u, u, 1).applyMatrix4(
+         y.clone().transpose().premultiply(Mh).multiply(MhT)
+      )
+   )
+
+   const hp = vp.dot(
+      new THREE.Vector4(u * u * u, u * u, u, 1).applyMatrix4(
+         h.clone().transpose().premultiply(Mh).multiply(MhT)
+      )
+   )
+
+   const sp = vp.dot(
+      new THREE.Vector4(u * u * u, u * u, u, 1).applyMatrix4(
+         s.clone().transpose().premultiply(Mh).multiply(MhT)
+      )
+   )
+
+   const lp = vp.dot(
+      new THREE.Vector4(u * u * u, u * u, u, 1).applyMatrix4(
+         l.clone().transpose().premultiply(Mh).multiply(MhT)
+      )
+   )
+
+   return {
+      vertex: [xp, yp, 0],
+      color: new THREE.Color().setHSL(hp, sp, lp).toArray(),
+   }
 }
 
-function download(next) {
-	shaderVertex = gl.createShader(gl.VERTEX_SHADER)
-	gl.shaderSource(shaderVertex, "attribute vec2 a_position; void main() { gl_Position = vec4(a_position, 0, 1);}")
-	gl.compileShader(shaderVertex)
+const divideGrid = (grid, divisions) => {
+   const newGrid = []
 
-	fetch(document.querySelector("#shader-gradient").src)
-		.then(function(res) {return res.text()})
-		.then(function(shaderGradientSource) {
-			shaderGradient = gl.createShader(gl.FRAGMENT_SHADER)
-			gl.shaderSource(shaderGradient, shaderGradientSource)
-			gl.compileShader(shaderGradient)
-			next()
-		})
+   for (let y = 0; y < height - 1; y++) {
+      for (let x = 0; x < width - 1; x++) {
+         const p00 = grid[y * height + x]
+         const p01 = grid[(y + 1) * height + x]
+         const p10 = grid[y * height + x + 1]
+         const p11 = grid[(y + 1) * height + x + 1]
+
+         const xp = new THREE.Matrix4()
+         // prettier-ignore
+         xp.set(
+            p00.x, p01.x, p00.v, p01.v,
+            p10.x, p11.x, p10.v, p11.v,
+            p00.u, p01.u, 0, 0,
+            p10.u, p11.u, 0, 0
+         )
+
+         const yp = new THREE.Matrix4()
+         // prettier-ignore
+         yp.set(
+            p00.y, p01.y, p00.v, p01.v,
+            p10.y, p11.y, p10.v, p11.v,
+            p00.u, p01.u, 0, 0,
+            p10.u, p11.u, 0, 0
+         )
+
+         const hp = new THREE.Matrix4()
+         // prettier-ignore
+         hp.set(
+            p00.h, p01.h, 0, 0,
+            p10.h, p11.h, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0
+         )
+
+         const sp = new THREE.Matrix4()
+         // prettier-ignore
+         sp.set(
+            p00.s, p01.s, 0, 0,
+            p10.s, p11.s, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0
+         )
+
+         const lp = new THREE.Matrix4()
+         // prettier-ignore
+         lp.set(
+            p00.l, p01.l, 0, 0,
+            p10.l, p11.l, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0
+         )
+
+         for (let u = 0; u < divisions; u++) {
+            for (let v = 0; v < divisions; v++) {
+               newGrid[
+                  (y * divisions + v) * dividedHeight + (x * divisions + u)
+               ] = dividePoint(
+                  u / (divisions - 1),
+                  v / (divisions - 1),
+                  xp,
+                  yp,
+                  hp,
+                  sp,
+                  lp
+               )
+            }
+         }
+      }
+   }
+
+   return newGrid
 }
 
-function setup() {
-	var buffer = gl.createBuffer()
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-	gl.bufferData(
-		gl.ARRAY_BUFFER,
-		new Float32Array([
-			-1.0, -1.0,
-			1.0, -1.0,
-			-1.0,  1.0,
-			-1.0,  1.0,
-			1.0, -1.0,
-			1.0,  1.0]),
-		gl.STATIC_DRAW
-	)
+const updateGeometry = (geometry, grid) => {
+   const vertices = []
+   const colors = []
 
-	var program = gl.createProgram()
-	gl.attachShader(program, shaderVertex)
-	gl.attachShader(program, shaderGradient)
-	gl.linkProgram(program)
-	gl.useProgram(program)
+   for (let y = 0; y < dividedHeight - 1; y++) {
+      for (let x = 0; x < dividedWidth - 1; x++) {
+         vertices.push(...grid[y * dividedHeight + x].vertex)
+         colors.push(...grid[y * dividedHeight + x].color)
 
-	var positionLocation = gl.getAttribLocation(program, "a_position")
-	widthHandle = gl.getUniformLocation(program, "width")
-	heightHandle = gl.getUniformLocation(program, "height")
-	seedHandle = gl.getUniformLocation(program, "seed")
-	gl.enableVertexAttribArray(positionLocation)
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0)
+         vertices.push(...grid[y * dividedHeight + x + 1].vertex)
+         colors.push(...grid[y * dividedHeight + x + 1].color)
 
-	update()
-	window.onresize = update
-	window.onkeyup = function(event) {
-		if (event.key === "Enter") {
-			if (looping) looping = false
-			else {
-				looping = true
-				requestAnimationFrame(loop)
-			}
-		}
-	}
+         vertices.push(...grid[(y + 1) * dividedHeight + x + 1].vertex)
+         colors.push(...grid[(y + 1) * dividedHeight + x + 1].color)
+
+         vertices.push(...grid[(y + 1) * dividedHeight + x + 1].vertex)
+         colors.push(...grid[(y + 1) * dividedHeight + x + 1].color)
+
+         vertices.push(...grid[(y + 1) * dividedHeight + x].vertex)
+         colors.push(...grid[(y + 1) * dividedHeight + x].color)
+
+         vertices.push(...grid[y * dividedHeight + x].vertex)
+         colors.push(...grid[y * dividedHeight + x].color)
+      }
+   }
+
+   geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(vertices), 3)
+   )
+   geometry.setAttribute(
+      "color",
+      new THREE.BufferAttribute(new Float32Array(colors), 3)
+   )
 }
 
-download(setup)
+const width = 3
+const height = 3
+const divisions = 10
+const dividedWidth = (width - 1) * divisions
+const dividedHeight = (height - 1) * divisions
+const grid = makeGrid(width, height)
+
+const scene = new THREE.Scene()
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000)
+camera.position.z = 5
+const renderer = new THREE.WebGLRenderer({
+   canvas: document.getElementById("canvas"),
+   alpha: true,
+})
+renderer.setSize(window.innerWidth + 400, window.innerHeight + 400)
+renderer.setScissor(200, 200, window.innerWidth, window.innerHeight)
+renderer.setScissorTest(true)
+const geometry = new THREE.BufferGeometry()
+const material = new THREE.MeshBasicMaterial({
+   color: 0xffffff,
+   vertexColors: true,
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const render = () => {
+   updateGeometry(geometry, divideGrid(grid, divisions))
+   renderer.render(scene, camera)
+}
+
+render()
